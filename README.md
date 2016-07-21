52d1fe82e0bc2fbdd0bb12ae7c306ba2039e1e2c
xyxiao <463618622@qq.com>
2016-07-21 17:23:10 +0800

    chang

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### 自己造轮子 自己用
+
+## [访问地址](http://xyxiao001.github.io/wheel)
---
+++ wheelList/barrelLayout/barrelLayout.js
@@ -0,0 +1,7 @@
+// 木桶布局代码
+//网页可见高度 document.body.clientWidth;
+var rows = document.createElement('div')
+var img = document.createElement('img')
+img.src = 'http://placehold.it/380x400'
+rows.appendChild(img)
+console.log(img)
