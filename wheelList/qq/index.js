d4dd1e0ef760f4010a96845eaf2cc3f6b42aae66
xyxiao <463618622@qq.com>
2016-08-15 09:26:58 +0800

    修复垂直居中移动问题

--- wheelList/qq/index.js
+++ wheelList/qq/index.js
@@ -1,13 +1,17 @@
+'use stricts';
 //拖动函数
 function tuo(el) {
+  var mousex, mousey, marginLeft;
   var move = function () {
-    el.style.left = event.x - mousex + 'px';
+    marginLeft = marginLeft.replace('px', '')
+    el.style.left = event.x - mousex - marginLeft + 'px';
     el.style.top = event.y - mousey + 'px';
   };
   el.addEventListener('mousedown', function () {
     el.style.transitionDuration = '0ms'
     mousex = event.x - el.offsetLeft;
     mousey = event.y - el.offsetTop;
+    marginLeft = window.getComputedStyle(el).marginLeft
     document.addEventListener('mousemove', move);
   });
   el.addEventListener('mouseup', function () {
