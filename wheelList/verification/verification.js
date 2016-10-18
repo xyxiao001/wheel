b5cfa03954fcc2f61b986c3329f983e5d30e5d18
xyxiao <463618622@qq.com>
2016-10-18 15:03:39 +0800

    add some

--- wheelList/verification/verification.js
+++ wheelList/verification/verification.js
@@ -2,11 +2,14 @@
 class Verification {
   constructor() {
     const self = ''
-    this.left = 5
+    this.x = 0
+    this.left = 180
+    this.end = 0
     this.group = document.querySelector('.v-group')
     this.bg = document.querySelector('.v-bg')
     this.control = document.querySelector('.s-control')
     this.text = document.querySelector('.s-text')
+    this.wStart = document.querySelector('.w-start')
     this.bg.addEventListener('click', this.hideBox)
     document.querySelector('.v-closed').addEventListener('click', this.hideBox)
     this.control.addEventListener('mousedown', this.go)
@@ -29,20 +32,35 @@ class Verification {
     self.addClass(self.group, 'v-hide')
   }
   // 开始移动函数
-  go () {
+  go (event) {
     self.addClass(self.control, 'v-go')
     self.addClass(self.text, 'fade-out')
     document.querySelector('body').addEventListener('mousemove', self.move)
+    self.x = event.clientX - event.offsetX
   }
   // 结束
   leave () {
     self.removeClass(self.control, 'v-go')
     self.removeClass(self.text, 'fade-out')
     document.querySelector('body').removeEventListener('mousemove', self.move)
+    if (self.end !== self.left) {
+      self.control.style.transitionDuration = '1s'
+      self.wStart.style.transitionDuration = '1s'
+      self.control.style.left = '0px'
+      self.wStart.style.left = '0px'
+    } else {
+      alert('验证通过')
+    }
   }
   // 移动
-  move() {
-    console.log(1)
+  move(event) {
+    self.control.style.transitionDuration = '0s'
+    self.wStart.style.transitionDuration = '0s'
+    self.end = ~~(event.clientX - self.x)
+    if (self.end > 0 && self.end < 220) {
+      self.control.style.left = self.end + 'px'
+      self.wStart.style.left = self.end + 'px'
+    }
   }
 
   //addClass  实现
