5d3fde9b98380681f91d45e2cd25cc37e406efad
xyxiao <463618622@qq.com>
2016-10-18 11:40:21 +0800

    add some

--- wheelList/verification/verification.js
+++ wheelList/verification/verification.js
@@ -3,9 +3,13 @@ class Verification {
   constructor() {
     const self = ''
     this.group = document.querySelector('.v-group')
-    this.bg =  document.querySelector('.v-bg')
+    this.bg = document.querySelector('.v-bg')
+    this.control = document.querySelector('.s-control')
+    this.text = document.querySelector('.s-text')
     this.bg.addEventListener('click', this.hideBox)
     document.querySelector('.v-closed').addEventListener('click', this.hideBox)
+    this.control.addEventListener('mousedown', this.go)
+    document.querySelector('body').addEventListener('mouseup', this.leave)
   }
   start() {
     self = this
@@ -14,25 +18,49 @@ class Verification {
   }
   hideBox() {
     self.removeClass(self.group, 'v-show')
+    self.addClass(self.group, 'a-hide')
+    document.querySelector('.a-hide').addEventListener('animationend', self.animateHide)
+  }
+  // 消失动画
+  animateHide () {
+    document.querySelector('.a-hide').removeEventListener('animationend', self.animateHide)
+    self.removeClass(self.group, 'a-hide')
     self.addClass(self.group, 'v-hide')
   }
+  // 开始移动函数
+  go () {
+    self.addClass(self.control, 'v-go')
+    self.addClass(self.text, 'fade-out')
+    document.querySelector('body').addEventListener('mousemove', self.move)
+  }
+  // 结束
+  leave () {
+    self.removeClass(self.control, 'v-go')
+    self.removeClass(self.text, 'fade-out')
+    document.querySelector('body').removeEventListener('mousemove', self.move)
+  }
+  // 移动
+  move() {
+    console.log(1)
+  }
+
   //addClass  实现
   addClass (el, name) {
-  // 获取到原有的class
-  var list = el.getAttribute('class').split(' ')
-  // 添加新的
-  var add = true
-  // 判断添加的是否存在
-  list.forEach(function (val, index) {
-    if (list[index] === name) {
-      add = false
+    // 获取到原有的class
+    var list = el.getAttribute('class').split(' ')
+    // 添加新的
+    var add = true
+    // 判断添加的是否存在
+    list.forEach(function (val, index) {
+      if (list[index] === name) {
+        add = false
+      }
+    })
+    if (add) {
+      list.push(name)
     }
-  })
-  if (add) {
-    list.push(name)
-  }
-  var newList = list.join(' ')
-  el.className = newList
+    var newList = list.join(' ')
+    el.className = newList
   }
 
 
