925c17d8cc242de1b14c08f3a98f8a31581ab20e
xyxiao <463618622@qq.com>
2017-02-08 17:44:46 +0800

    add some

--- wheelList/map-gaode/js/index.js
+++ wheelList/map-gaode/js/index.js
@@ -1,12 +1,11 @@
 'use strict'
-// 地图
 //地图加载
 var map = new AMap.Map("l-map", {
     resizeEnable: true
 });
 //输入提示
 var autoOptions = {
-    input: "tipinput"
+    input: "suggestId"
 };
 var auto = new AMap.Autocomplete(autoOptions);
 var placeSearch = new AMap.PlaceSearch({
@@ -18,7 +17,35 @@ function select(e) {
     placeSearch.search(e.poi.name);  //关键字查询查询
 }
 
+map.plugin('AMap.Geolocation', function() {
+    var geolocation = new AMap.Geolocation({
+        enableHighAccuracy: true,//是否使用高精度定位，默认:true
+        timeout: 10000,          //超过10秒后停止定位，默认：无穷大
+        buttonOffset: new AMap.Pixel(15, 140),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+        zoomToAccuracy: true,      //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+        buttonPosition:'RB'
+    });
+    map.addControl(geolocation);
+    geolocation.getCurrentPosition();
+    AMap.event.addListener(geolocation, 'complete', onComplete);//返回定位信息
+    AMap.event.addListener(geolocation, 'error', onError);      //返回定位出错信息
+});
+//解析定位结果
+function onComplete(data) {
+    var str=['定位成功'];
+    str.push('经度：' + data.position.getLng());
+    str.push('纬度：' + data.position.getLat());
+    if(data.accuracy){
+         str.push('精度：' + data.accuracy + ' 米');
+    }//如为IP精确定位结果则没有精度信息
+    str.push('是否经过偏移：' + (data.isConverted ? '是' : '否'));
+}
+//解析定位错误信息
+function onError(data) {
+    alert('定位失败');
+}
 
+// 表单校验
 var query = function (i) {
   return document.querySelector(i)
 }
