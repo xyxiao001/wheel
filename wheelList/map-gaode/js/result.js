8d8abe99a5187eb3da705a9001a1fa6a038f7d16
xyxiao <463618622@qq.com>
2017-02-09 10:59:27 +0800

    修改高德地图

---
+++ wheelList/map-gaode/js/result.js
@@ -0,0 +1,23 @@
+var query = function (i) {
+  return document.querySelector(i);
+}
+//地图加载
+var map = new AMap.Map("l-map", {
+    resizeEnable: true
+});
+var lnglatXY = [120.1718859494253,30.264250329885062];
+var geocoder = new AMap.Geocoder({});
+geocoder.getAddress(lnglatXY, function(status, result) {
+    if (status === 'complete' && result.info === 'OK') {
+      query('#result-adress').value = result.regeocode.formattedAddress
+    }
+});
+
+var marker = new AMap.Marker({  //加点
+    map: map,
+    draggable: false, // 是否可以拖拽,
+    position: lnglatXY
+});
+map.setFitView();
+
+query('#demand').value = '希望知道下城区长庆街道青云街47号现在堵不堵车。。'
